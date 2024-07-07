1b5b0f690a52ed945ba663b6f49879d22dcd2db1
Roman Volkov <rokimarux@gmail.com>
2024-07-08 00:38:03 +0500

    Initial commit

---
+++ hangMan.md
@@ -0,0 +1,22 @@
+### What must be done?
+
+1. Name request
+2. Greatings
+3. Word generation / randomization
+4. Word encryption - counting number of letters, inserting "\_" instead of letters
+5. sending message to player with letter request
+6. If player is right then needed leeter must be reviled
+7. If not then the number of one of 5 remaining attepts must be rediced
+8. If player will spend all 5 attepts then they lost
+9. If player will unravel all the letters then they win
+
+### additional features for future
+
+1. Possibility to choose a topic
+2. Keyboard Controls
+3. Hearts instead of attempts counter +++
+4. PixelARt ONLY design +++
+5. "Play again" Button +++
+6. More icons for everything +++
+7. Buy me a cofe
+8. visual display of locked buttons after the end of the game
---
+++ hangman.js
@@ -0,0 +1,179 @@
+const hearts = document.getElementById("heartsConteainer");
+const wordStatus = document.getElementById("wordStatus");
+const gmText = document.getElementById("gmText");
+const input = document.getElementById("input");
+const enterButton = document.getElementById("enter");
+const restartButton = document.getElementById("restart");
+const clueButton = document.getElementById("clue");
+
+let randomWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+console.log("randomWord: " + randomWord);
+
+let remainingLetters = randomWord.length;
+let defaultAttempts = 6;
+let remainingAttempts = defaultAttempts;
+
+let randomWordArray = [];
+for (let i = 0; i < randomWord.length; i++) {
+  randomWordArray.push(randomWord[i]);
+}
+console.log("randomWordArray: " + randomWordArray);
+
+let riddledWordArray = [];
+for (let i = 0; i < randomWord.length; i++) {
+  riddledWordArray.push("_");
+}
+
+function updateWordStatus() {
+  wordStatus.innerHTML = riddledWordArray.join("");
+}
+function updateGmText(text) {
+  gmText.innerHTML = text;
+}
+
+input.focus();
+heartsRendering();
+updateWordStatus();
+updateGmText(
+  `Guess the word before you run out of attempts. Here is only one clue.` +
+    `<br>` +
+    `Enter one letter at a time.`
+);
+
+console.log("riddledWordArray: " + riddledWordArray);
+
+const wrongAnswers = [];
+
+enterButton.onclick = function () {
+  const guess = input.value.toLowerCase();
+  input.value = "";
+  input.focus();
+  checkGuess(guess);
+};
+restartButton.onclick = function () {
+  location.reload();
+};
+
+function checkGuess(guess) {
+  if (
+    randomWordArray.indexOf(guess.toLowerCase()) === -1 &&
+    wrongAnswers.indexOf(guess.toLowerCase()) === -1
+  ) {
+    // New incorrect guess
+    updateGmText("There is no such letter.");
+    wrongAnswers.push(guess.toLowerCase());
+    remainingAttempts--;
+  } else if (
+    randomWordArray.indexOf(guess.toLowerCase()) === -1 &&
+    wrongAnswers.indexOf(guess.toLowerCase()) !== -1
+  ) {
+    // Old incorrect guess
+    updateGmText("Sorry, but we already tried this letter.");
+  } else if (riddledWordArray.indexOf(guess) !== -1) {
+    // Letter already guessed correctly
+    updateGmText(`We already got letter "${guess.toUpperCase()}".`);
+  } else {
+    // Correct guess, update the riddledWordArray
+    updateGmText(`Nice! There is letter "${guess.toUpperCase()}"`);
+    for (let j = 0; j < randomWord.length; j++) {
+      if (randomWord[j] === guess.toLowerCase()) {
+        riddledWordArray[j] = guess.toLowerCase();
+        remainingLetters--;
+      }
+    }
+  }
+  updateWordStatus();
+  heartsRendering();
+
+  if (remainingLetters === 0) {
+    updateGmText("Congratulations! You guessed the word!");
+    input.disabled = true;
+    input.placeholder = `Nice round`;
+    enterButton.disabled = true;
+    clueButton.disabled = true;
+  } else if (remainingAttempts === 0) {
+    updateGmText(
+      `Sorry, you've run out of attempts. The word was ${randomWord.toUpperCase()}.`
+    );
+    input.disabled = true;
+    input.placeholder = `Nice round`;
+    enterButton.disabled = true;
+    clueButton.disabled = true;
+  }
+}
+
+let numberOfClues = 0;
+clueButton.onclick = function () {
+  if (numberOfClues < 1) {
+    let randomNumberOfLetter;
+    let randomLetter;
+
+    do {
+      randomNumberOfLetter = Math.floor(Math.random() * randomWord.length);
+      randomLetter = randomWord[randomNumberOfLetter];
+    } while (riddledWordArray[randomNumberOfLetter] !== "_");
+    input.value = randomLetter;
+    clueButton.disabled = true;
+    numberOfClues++;
+  } else {
+    updateGmText("You can only use the clue once.");
+  }
+};
+
+console.log(numberOfClues);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    enterButton.onclick();
+  }
+  if (event.key === "Shift") {
+    clueButton.onclick();
+  }
+  if (event.key === "Space") {
+    restartButton.onclick();
+  }
+});
+
+function filledHeart() {
+  return `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          class="heart"
+        >
+          <rect width="24" height="24" fill="none" />
+          <path
+            fill="currentColor"
+            d="M5 2V3.99997H3V6H1V12H3V14H5V16H6.99998V18H9.00001V20H11V22H13V20H15V18H17V16H19V14H19.0175H21V12H23V6H21V3.99997H19.0175H19V2H15V3.99997H13V6H11V3.99997H9.03317H9.00001V2H5Z"
+          />
+        </svg>
+`;
+}
+function emptyHeart() {
+  return `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          class="heart"
+        >
+          <rect width="24" height="24" fill="none" />
+          <path
+            fill="currentColor"
+            d="M9 2H5v2H3v2H1v6h2v2h2v2h2v2h2v2h2v2h2v-2h2v-2h2v-2h2v-2h2v-2h2V6h-2V4h-2V2h-4v2h-2v2h-2V4H9zm0 2v2h2v2h2V6h2V4h4v2h2v6h-2v2h-2v2h-2v2h-2v2h-2v-2H9v-2H7v-2H5v-2H3V6h2V4z"
+          />
+        </svg>
+`;
+}
+function heartsRendering() {
+  hearts.innerHTML = "";
+  for (i = 1; i <= remainingAttempts; i++) {
+    hearts.insertAdjacentHTML("beforeend", filledHeart());
+  }
+  for (i = remainingAttempts + 1; i <= defaultAttempts; i++) {
+    hearts.insertAdjacentHTML("beforeend", emptyHeart());
+  }
+}
